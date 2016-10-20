28b59f4ef72b3cbdf3bb8ac169c4959ddd54fe6e
Conner-Stack <connerstack@gmail.com>
2016-10-20 17:35:44 -0500

    push
    ughhh

---
+++ Tanks tut/Assets/Scripts/Test.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+    public Color m_color;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
